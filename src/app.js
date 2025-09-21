60640d5f4464fa86de7869d812209e921b8e4260
TiwariSudhanshu <sudhanshutiwari9836@gmail.com>
2025-09-21 12:48:56 +0530

    Updated cors

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'https://rumin-five.vercel.app'],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
 }));
